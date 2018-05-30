56f2bee2cbe0ee91b4640e4e9deaa1cf61c91442
Casey Lopez <casey@sobrilsky.com>
2018-05-30 09:30:06 -0500

    "transferred to post office" != delivered

--- lib/omniship/ups/track/package.rb
+++ lib/omniship/ups/track/package.rb
@@ -16,8 +16,17 @@ module Omniship
         end
 
         def alternate_tracking
-          path = @root.xpath('AlternateTrackingInfo')
-          AlternateTracking.new(path) if path
+          if !@root.xpath('PackageServiceOptions/USPSPICNumber').empty?
+            # surepost
+            path = @root.xpath('PackageServiceOptions/USPSPICNumber').text
+            puts "USPSPICNumber #{path}"
+          else
+            # mail innovations
+            path = @root.xpath('AlternateTrackingInfo')
+            puts "non USPSPICNumber = #{path.to_xml.inspect}"
+          end
+
+          AlternateTracking.new(path) if !path.empty?
         end
 
         def has_left?
@@ -25,7 +34,7 @@ module Omniship
         end
 
         def has_arrived?
-          activity.any? {|activity| activity.code == ARRIVAL_CODE }
+          activity.any? {|activity| activity.code == ARRIVAL_CODE && !activity.status.include?("transferred to post office")}
         end
       end
     end
