5a396f636850db214e72a5cc24cd45f2b947a4a7
Kevin Solkowski <ksolkowski@gmail.com>
2022-10-26 16:06:18 -0500

    more more more

--- lib/omniship/usps/track/package.rb
+++ lib/omniship/usps/track/package.rb
@@ -2,6 +2,27 @@ module Omniship
   module USPS
     module Track
       class Package < Omniship::Base
+        # https://about.usps.com/publications/pub97/pub97_appi.htm
+        HAS_LEFT_CODES = [
+          "SF", # UNDOCUMENTED but seems to be "Acceptance" (Departed Post Office)
+          "03", # Accept or Pickup (by carrier)
+          "10", # Processed
+          "80", # Picked Up by Shipping Partner
+          "81", # Arrived Shipping Partner Facility
+          "82", # Departed Shipping Partner Facility
+          "16", # Available for Returns Agent
+          "MR", # UNDOCUMENTED Picked Up and Processed by Agent
+          "OF", # When a mailpiece receives the physical scan “arrival at unit”
+          "DX", # If no delivery scan made 14 hours after OF status. (Awaiting Delivery Scan)
+          "07", # Arrival at Unit
+          "PC"  # Sorting / Processing Complete - After a mailpiece receives the “07 Arrival at Unit” scan
+        ].freeze
+
+        HAS_ARRIVED_CODES = [
+          "01", # Delivered*
+          "17", # Picked Up By Agent, Tendered to Returns Agent
+          "MR"  # UNDOCUMENTED Picked Up and Processed by Agent
+        ].freeze
 
         def tracking_number
           @root.attribute("ID").to_s
@@ -17,32 +38,14 @@ module Omniship
         end
 
         # this is actually an indicator that the the package has been scanned by USPS ANYWHERE
-        # https://about.usps.com/publications/pub97/pub97_i.htm
         def has_left?
-          has_arrived? or
-          activity.any? {|activity|
-            [
-              "SF", # UNDOCUMENTED but seems to be "Acceptance"
-              "03", # Accept or Pickup (by carrier)
-              "10", # Processed
-              "80", # Picked Up by Shipping Partner
-              "81", # Arrived Shipping Partner Facility
-              "82",
-              "16", # Available for Returns Agent
-              "MR" # UNDOCUMENTED Picked Up and Processed by Agent
-            ].include?(activity.code) # Departed Shipping Partner Facility
-          }
+          has_arrived? ||
+            activity.any? { |activity| HAS_LEFT_CODES.include?(activity.code) }
         end
 
+        # delivered or ready for pickup at post office, or Notice Left
         def has_arrived?
-          # http://about.usps.com/publications/pub97/pub97_i.htm
-          activity.any? {|activity|
-            # deliverred or ready for pickup at post office, or Notice Left
-            ["01", # Delivered*
-              "17", # Picked Up By Agent, Tendered to Returns Agent
-              "MR" # UNDOCUMENTED Picked Up and Processed by Agent
-            ].include?(activity.code)
-          }
+          activity.any? { |activity| HAS_ARRIVED_CODES.include?(activity.code) }
         end
 
         def scheduled_delivery
